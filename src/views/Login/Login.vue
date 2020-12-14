d487afd48893e5b5d926a0817ece55c026a14ccc
hongyisen <1800013132@pku.edu.cn>
2020-12-15 01:12:33 +0800

    把login的表单改为elementUI风格，添加了对输入规范的检查

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,26 +1,66 @@
 <template>
   <div id="login">
     <h1>Schedule note 用户登录</h1>
-    <form>
-        <div id="input">
-            账号
-            <input type="text" name="userid" placeholder="请输入你的账号">
-            <br>
-            密码
-            <input type="password" name="userpassword" placeholder="请输入你的密码">
-            <br>
-            <h1></h1>
-            <button v-on:click="loginCheck()">登录</button>
-            <h1></h1>
-            <router-link to="/register">注册账号</router-link>
-        </div>
-    </form>
+    <el-form ref="form" :rules="rules" :model="form" label-width="80px" class="login-form">
+
+        <el-form-item label="学工号" prop="username">
+          <el-input v-model.number="form.username"></el-input>
+        </el-form-item>
+
+        <el-form-item label="密码" prop="password">
+          <el-input v-model="form.password"></el-input>
+        </el-form-item>
+
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">登录</el-button>
+        </el-form-item>
+      </el-form>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    const validateNumber = (rule, value, callback) => {
+      console.log(typeof (value))
+      console.log(value > 1)
+      if (!value) {
+        callback(new Error('学工号不能为空'))
+      } else if (typeof (value) !== 'number') {
+        callback(new Error('学工号应该是数字'))
+      } else if (value < 1000000000 || value > 9999999999) {
+        callback(new Error('请输入10位数学工号'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { type: 'number', validator: validateNumber }
+        ],
+        password: [
+          { required: true, message: '密码不能为空', trigger: 'blur' },
+          { min: 7, max: 15, message: '密码7-15位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
   methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
     register: function () {
       this.$router.push('/register')
     },
@@ -29,4 +69,5 @@ export default {
     }
   }
 }
+
 </script>
