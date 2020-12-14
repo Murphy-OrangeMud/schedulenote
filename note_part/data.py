c168a3158fc6cff1710c04bbc6cf8aeca4a77aa5
wuxigk <wuxigk@github.com>
2020-12-14 20:31:42 +0800

    Note part init

---
+++ note_part/data.py
@@ -0,0 +1,83 @@
+from flask import request
+from flask import jsonify
+from flask import send_from_directory, make_response
+from flask import Blueprint
+import os
+import markdown
+import pdfkit
+
+from dataStructure import Note
+from dataStructure import db
+
+app = Blueprint("note", __name__, url_prefix='/note')
+
+
+@app.route("/modifyNotes", methods=["POST"])
+def modityNotes():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        newSourceCode = json["sourceCode"]
+        note = Note.query.get(ID)
+        note.sourceCode = newSourceCode
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@app.route("/previewNote", methods=["POST"])
+def previewNote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        return jsonify({"status": "OK", "html": markdown.Markdown(Note.query.get(ID).sourceCode)})
+
+
+@app.route("/exportPDF", methods=["POST"])
+def exportPDF():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        path = 'pdfGenerated/'
+        fileName = ID + '.pdf'
+        pdfkit.from_string(markdown.Markdown(Note.query.get(ID).sourceCode), path + fileName)
+        return make_response(send_from_directory(os.path.join(app.root_path, path), fileName, as_attachment=True))
+
+
+@app.route("/newNote", methods=["POST"])
+def newNote():
+    if request.method == "POST":
+        json = request.get_json()
+        newNote = Note(json["sourceCode"], json["owner"], json["course_belonged"])
+        db.session.add(newNote)
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@app.route("/upVote", methods=["POST"])
+def upVote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        note = Note.query.get(ID)
+        note.sourceCode += 1
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@app.route("/downVote", methods=["POST"])
+def downVote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        note = Note.query.get(ID)
+        note.sourceCode -= 1
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@app.route("/getUps", methods=["Get"])
+def getUps():
+    if request.method == "GET":
+        json = request.get_json()
+        ID = json["ID"]
+        return jsonify({"status": "OK", "ups": Note.query.get(ID).ups})
---
+++ note_part/dataStructure.py
@@ -0,0 +1,24 @@
+from flask import current_app
+from flask_sqlalchemy import SQLAlchemy
+import uuid
+import datetime
+
+db = SQLAlchemy(current_app)
+
+class Note(db.Model):
+    id = db.Column(db.String, primary_key=True, unique=True)
+    sourceCode = db.Column(db.String)
+    owner = db.Column(db.String)
+    createTime = db.Column(db.DateTime, default=datetime.datetime.now)
+    modifyTime = db.Column(db.DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+    courseBelonged = db.Column(db.String)
+    ups = db.Column(db.Integer, default=0)
+
+    def __init__(self, source_code, owner, course_belonged):
+        self.id = uuid.uuid1()
+        self.sourceCode = source_code
+        self.owner = owner
+        self.courseBelonged = course_belonged
+
+
+db.create_all()
