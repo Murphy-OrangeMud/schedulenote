8345e7fc91ee4be9c6fb38105f3bb9ccc335a69c
jiang13021 <45600652+jiang13021@users.noreply.github.com>
2020-12-26 14:05:10 +0800

    Add admin_bp

--- backend/__init__.py
+++ backend/__init__.py
@@ -14,12 +14,12 @@ def create_app():
     if not os.path.exists(IMAGEPATH):
         os.makedirs(IMAGEPATH)
     with app.app_context():
-        from .api import login_manager, user_bp, schedule_bp, note_bp, course_bp
+        from .api import login_manager, user_bp, schedule_bp, note_bp, course_bp, admin_bp
         # db.create_all()
         login_manager.init_app(app)
         app.register_blueprint(user_bp)
         app.register_blueprint(schedule_bp)
         app.register_blueprint(note_bp)
         app.register_blueprint(course_bp)
-        
+        app.register_blueprint(admin_bp)
     return app
