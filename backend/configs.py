5cd7b9a28ccb483510fe6e3c0d436d24aad75d86
jiang13021 <45600652+jiang13021@users.noreply.github.com>
2020-12-26 13:54:36 +0800

    Add configs REDISHOST ……

--- backend/configs.py
+++ backend/configs.py
@@ -1,17 +1,11 @@
-# secret.py
-HOST = 'localhost'
-PORT = '3306'
-DATABASE = 'usertest'
-USERNAME = 'root'
-PASSWORD = ''
-
-MAIL_PASSWORD = 'EUJTGGGIQFXWGFUJ'
-SECRET_KEY = 'FOOLISH_KEY'
+from secret import *
 
 # Size Limits
 MAXSTRLEN = 64 #username和password的最大值
 MAXMOTTO = 64
 MAXAVATAR = 64
+PSW_HASH_LEN = 128
+MAXMSG = 200
 
 # Path Configs
 ROOTPATH = "/mnt/c/Users/cheng/schedulenote/"
@@ -19,13 +13,14 @@ IMAGEPATH = ROOTPATH + "images/"
 
 #Redis Configs
 REDIS_STAY_TIME = 300 #Redis中持续留存300s
+REDISHOST = "localhost"
+REDISPORT = 6379
 
 # Mail Configs
 MAIL_PORT = 465 #use SSL
 MAIL_SERVER = "smtp.163.com"
 MAIL_USEERNAME = 'schedulenote@163.com'
 MAIL_PASSWORD = MAIL_PASSWORD
-
 # App Configs
 DB_URI = "mysql://{username}:{password}@{host}:{port}/{db}?charset=utf8".format(username=USERNAME,password=PASSWORD, host=HOST,port=PORT, db=DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
