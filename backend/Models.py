bad65459767db70edbda194769bdfef2db5aef7f
jiang13021 <45600652+jiang13021@users.noreply.github.com>
2020-12-26 15:56:41 +0800

    Change User.isAdmin, Feedback.finished, Report.finished from Boolean to Integer

--- backend/Models.py
+++ backend/Models.py
@@ -258,27 +258,27 @@ class Course(db.Model):
 
 # User part
 class User(UserMixin, db.Model):
-    __tablename__ = "users_v2"
+    __tablename__ = "users_v3"
 
     id = db.Column(db.Integer,primary_key=True, nullable=False)
+    isAdmin = db.Column(db.Integer, nullable = False)
     username = db.Column(db.String(MAXSTRLEN), unique = True,  nullable=False)
     email = db.Column(db.String(MAXSTRLEN),  unique = True, nullable=False)
     password= db.Column(db.String(PSW_HASH_LEN), nullable = False)
     avatar = db.Column(db.String(MAXAVATAR), nullable = True)
     motto = db.Column(db.String(MAXMOTTO), nullable = True)
-    is_admin = db.Column(db.Boolean, nullable = False)
     def __init__(self, username, password, email):
         self.username = username
         self.email = email
         self.password = generate_password_hash(password)
         self.avatar = None
         self.motto = None
-        self.is_admin = False
+        self.isAdmin = 0
     def __repr__(self):
         return '<User %r>' % self.username
 
     def is_admin(self):
-        return self.is_admin
+        return self.isAdmin != 0
 
     #以dict的形式返回user的全部信息（除了password）
     def todict(self):
@@ -286,7 +286,7 @@ class User(UserMixin, db.Model):
         dic['id'] = self.id
         dic['username'] = self.username
         dic['email'] = self.email
-        dic['is_admin'] = int(self.is_admin)
+        dic['is_admin'] = self.isAdmin
         #将文件编程base64图片流传给前端
         dic['avatar'] = {}
         if self.avatar == None:
@@ -311,7 +311,7 @@ to_report_type = ["username", "avatar", "motto", "note"]
 class Report(db.Model):
     __tablename__ = "report"
     id = db.Column(db.Integer,primary_key=True, nullable=False)
-    finished = db.Column(db.Boolean, nullable = False)
+    finished = db.Column(db.Integer, nullable = False)
     reported_id = db.Column(db.Integer, nullable=False) #被举报者
     #被举报类型，包括昵称、头像、座右铭、笔记文件四种，分别用0、1、2、3代表
     to_report = db.Column(db.Integer, nullable=False) 
@@ -324,33 +324,35 @@ class Report(db.Model):
         self.msg = msg
         self.file_id = file_id
         self.whistleBlower_id = whistleBlower
-        self.finished = False
+        self.finished = 0
 
     def todict(self):
         dic = {}
         dic['id'] = self.id
-        dic['finished'] = int(self.finished)
+        dic['finished'] = self.finished
         dic['reported_id'] = self.reported_id
         dic['to_report'] = to_report_type[self.to_report]
         dic['msg'] = self.msg
         dic['file_id'] = self.file_id
         dic['whistleBlower_id'] = self.whistleBlower_id
+        return dic
 
 class Feedback(db.Model):
     __tablename__ = "feedback"
     id = db.Column(db.Integer,primary_key=True, nullable=False)
-    finished = db.Column(db.Boolean, nullable = False)
+    finished = db.Column(db.Integer, nullable = False)
     msg = db.Column(db.String(MAXMSG), nullable = False)
     whistleBlower_id = db.Column(db.Integer, nullable=True) #提意见的用户
     
     def __init__(self, msg, whistleBlower = -1):
         self.msg = msg
         self.whistleBlower_id = whistleBlower
-        self.finished = False
+        self.finished = 0
 
     def todict(self):
         dic = {}
         dic['id'] = self.id
-        dic['finished'] = int(self.finished)
+        dic['finished'] = self.finished
         dic['msg'] = self.msg
         dic['whistleBlower_id'] = self.whistleBlower_id
+        return dic
