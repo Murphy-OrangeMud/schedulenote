7e93934056a6597a52afe8d6f51c6045416aa9e6
Murphy-OrangeMud <chengzhiyi_2@163.com>
2021-01-02 00:41:53 +0800

    merge note part

--- backend/api.py
+++ backend/api.py
@@ -27,7 +27,7 @@ login_manager = LoginManager()
 
 
 # for test
-@current_app.route("/")
+@current_note_bp.route("/")
 def hello():
     return render_template("helloworld.html")
 
@@ -196,6 +196,77 @@ def modityNotes():
         note.sourceCode = newSourceCode
         db.session.commit()
         return jsonify({"status": "OK"})
+
+@note_bp.route("/exportPDF", methods=["POST"])
+def exportPDF():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        path = 'pdfGenerated/'
+        fileName = ID + '.pdf'
+        note = Note.query.get(ID)
+        if note==None:
+            return jsonify({"status": "ERR"})
+        pdfkit.from_string(markdown.markdown(note.sourceCode), "test")
+        return make_response(send_from_directory(os.path.join(note_bp.root_path, path), fileName, as_attachment=True))
+
+
+@note_bp.route("/newNote", methods=["POST"])
+def newNote():
+    if request.method == "POST":
+        json = request.get_json()
+        newNote = Note(json["sourceCode"], json["owner"], json["course_belonged"])
+        db.session.add(newNote)
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@note_bp.route("/upVote", methods=["POST"])
+def upVote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        note = Note.query.get(ID)
+        if note==None:
+            return jsonify({"status": "ERR"})
+        note.ups += 1
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@note_bp.route("/downVote", methods=["POST"])
+def downVote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        note = Note.query.get(ID)
+        if note==None:
+            return jsonify({"status": "ERR"})
+        note.ups -= 1
+        db.session.commit()
+        return jsonify({"status": "OK"})
+
+
+@note_bp.route("/getNote", methods=["POST"])
+def getNote():
+    if request.method == "POST":
+        json = request.get_json()
+        ID = json["ID"]
+        note = Note.query.get(ID)
+        if note==None:
+            return jsonify({"status": "ERR"})
+        return jsonify(
+            {"sourceCode": note.sourceCode, "owner": note.owner, "createTime": note.createTime,
+             "modifyTime": note.modifyTime, "courseBelonged": note.courseBelonged, "ups": note.ups})
+
+
+@note_bp.route("/getAllNoteID", methods=["GET"])
+def getAllNoteID():
+    if request.method == "GET":
+        IDList = []
+        for note in Note.query.all():
+            IDList.append(note.id)
+        return jsonify(IDList)
 
 
 @note_bp.route("/previewNote", methods=["POST"])
