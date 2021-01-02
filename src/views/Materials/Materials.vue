288936b63ad3cf3398fe7a0284e1606b07210ede
tyfeld <42889742+tyfeld@users.noreply.github.com>
2021-01-02 21:30:22 +0800

    adjust courseid

--- src/views/Materials/Materials.vue
+++ src/views/Materials/Materials.vue
@@ -114,7 +114,7 @@ export default {
           return
         }
         var formData = new FormData()
-        formData.append('course', 2)
+        formData.append('course', this.$store.state.courseid)
         formData.append('uploader', 2)
         formData.append('description', 'hzj')
         formData.append('file', this.addArr[i])
